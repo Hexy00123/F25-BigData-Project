fcd34626555a8513125175a79ef30c5d10e2830e
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2025-04-04 16:39:21 +0300

    fix: stage1: uncommented commands

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -3,10 +3,10 @@ url="https://www.kaggle.com/api/v1/datasets/download/elemento/nyc-yellow-taxi-tr
 data_dir=$(pwd)/data
 
 echo "INFO: Downloading the data from source..."
-# curl -L -o "$data_dir/nyc-yellow-taxi-trip-data.zip" "$url"
+curl -L -o "$data_dir/nyc-yellow-taxi-trip-data.zip" "$url"
 
 echo "INFO: Unzipping..."
 unzip -p $data_dir/nyc-yellow-taxi-trip-data.zip yellow_tripdata_2016-03.csv > $data_dir/dataset.csv
 
 echo "INFO: Clear files..."
-# rm $data_dir/nyc-yellow-taxi-trip-data.zip
+rm $data_dir/nyc-yellow-taxi-trip-data.zip
