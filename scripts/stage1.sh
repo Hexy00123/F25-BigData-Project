e668b5ecb5867e11ee2983db2537a3b93dbcd0b2
IU cluster <team11@hadoop-01.uni.innopolis.ru>
2025-04-05 20:59:24 +0300

    fix: stage1: moving *.avsc & *.java files to output dir

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+bash scripts/data_collection.sh
+bash scripts/data_storage.sh
+password=$(head -n 1 secrets/.psql.pass)
+
+echo "INFO: Clear hdfs warehouse folder"
+hdfs dfs -rm -r project/warehouse/taxi_trips
+
+echo "INFO: Importing data via sqoop"
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team11_projectdb --username team11 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
+
+echo "INFO: Move *.avsc & *.java to output"
+mv *.avsc output/
+mv *.java output/
