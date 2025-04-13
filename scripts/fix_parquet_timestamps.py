574888073e560a2f48d485c3d23e0210318f4908
Nursultan Abdullaev <117067651+nabdullaev@users.noreply.github.com>
2025-04-13 14:12:27 +0300

    feat: Spark timestamps processing

---
+++ scripts/fix_parquet_timestamps.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder \
+    .appName("Fix Timestamps for Hive") \
+    .getOrCreate()
+
+df = spark.read.parquet("hdfs://hadoop-02.uni.innopolis.ru:8020/user/team11/project/warehouse/taxi_trips")
+
+df_fixed = df.withColumn("tpep_pickup_datetime", col("tpep_pickup_datetime").cast("string")) \
+             .withColumn("tpep_dropoff_datetime", col("tpep_dropoff_datetime").cast("string"))
+
+df_fixed.write.mode("overwrite").parquet("hdfs://hadoop-02.uni.innopolis.ru:8020/user/team11/project/warehouse/taxi_trips_fixed")
