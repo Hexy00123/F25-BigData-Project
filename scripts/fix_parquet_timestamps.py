e59ea808c672566d2905ad6518daa34880826019
Brvcket <ahmatovbulat@gmail.com>
2025-05-06 15:58:18 +0300

    fix: pylint for fix_parquet_timestamps.py

--- scripts/fix_parquet_timestamps.py
+++ scripts/fix_parquet_timestamps.py
@@ -1,14 +1,17 @@
+"""Fix of timestamp formats in parquet"""
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+
+OLD_PATH = "hdfs://hadoop-02.uni.innopolis.ru:8020/user/team11/project/warehouse/taxi_trips"
+FIXED_PATH = "hdfs://hadoop-02.uni.innopolis.ru:8020/user/team11/project/warehouse/taxi_trips_fixed"
 
 spark = SparkSession.builder \
     .appName("Fix Timestamps for Hive") \
     .getOrCreate()
 
-df = spark.read.parquet("hdfs://hadoop-02.uni.innopolis.ru:8020/user/team11/project/warehouse/taxi_trips")
+df = spark.read.parquet(OLD_PATH)
 
 df_fixed = df.withColumn("tpep_pickup_datetime", col("tpep_pickup_datetime").cast("string")) \
              .withColumn("tpep_dropoff_datetime", col("tpep_dropoff_datetime").cast("string"))
 
-df_fixed.write.mode("overwrite").parquet("hdfs://hadoop-02.uni.innopolis.ru:8020/user/team11/project/warehouse/taxi_trips_fixed")
-
+df_fixed.write.mode("overwrite").parquet(FIXED_PATH)
